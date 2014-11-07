f29de1869975c6578eeaf145519bf619466db97e
Tom Chen <developer@tomchentw.com>
2014-11-07 21:39:39 +0800

    chore(test): add test case for linters

---
+++ test/index.js
@@ -0,0 +1,8 @@
+"use strict";
+/**
+ * __template__gist__: https://gist.github.com/tomchentw/0dc24c30955c1a6c94d4#file-test__index-js
+ */
+describe("koa-webpack-dev", function() {
+  require("mocha-jshint")(["./lib/"]);
+  require("mocha-jscs")(["./lib/"]);
+});
